3976eb87dcf19c8854cd9c649b312b00420a0849
nameless-H <s.hasan.ayub@gmail.com>
2026-07-11 03:39:03 +0500

    Refactor: Changed code structure to a state machine

--- Fundamentals/blink-test/src/main.cpp
+++ Fundamentals/blink-test/src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
 
+enum LedState { LED_OFF, LED_ON };
+LedState currentState = LED_OFF;
 
+unsigned long lastTransitionTime = 0;
+const unsigned long blinkInterval = 1000; // ms
+const int ledPin = 13;
 
 void setup() {
-  pinMode(8, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(8, HIGH);
-  delay(200);
-  digitalWrite(8, LOW);
-  delay(800);
+  unsigned long now = millis();
+
+  if (now - lastTransitionTime >= blinkInterval) {
+    lastTransitionTime = now;
+
+    switch (currentState) {
+      case LED_OFF:
+        currentState = LED_ON;
+        digitalWrite(ledPin, HIGH);
+        break;
+      case LED_ON:
+        currentState = LED_OFF;
+        digitalWrite(ledPin, LOW);
+        break;
+    }
+  }
 }
